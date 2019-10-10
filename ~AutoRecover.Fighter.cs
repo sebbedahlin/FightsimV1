efcd477175c9dd7da3fc3f603b749c296221f8ad
sebbedahlin <sebbedahlin@gmail.com>
2019-10-10 13:26:46 +0200

    1

---
+++ ~AutoRecover.Fighter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightsimV1
+{
+    class Fighter
+    {
+
+        private int hp = 100;
+        public string name;
+
+        int Attack
+        {
+
+        }
+
+        void Hurt
+        {
+
+        }
+
+        bool IsAlive
+        {
+
+        }
+
+        int GetHp
+        {
+
+        }
+    }
+}
---
+++ ~AutoRecover.Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightsimV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+
+            Console.Clear();
+
+            Fighter A = new Fighter();
+            Fighter B = new Fighter();
+
+            playername:
+
+            Console.WriteLine("Välj namn på fighter A och fighter B:");
+
+            string Fighter A = Console.ReadLine();
+
+            while ()
+
+
+
+
+            
+        }
+    }
+}
